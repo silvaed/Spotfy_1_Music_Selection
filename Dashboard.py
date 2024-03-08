e4dcfc1f438f36bff620aebcecb3f11a7cf546b4
silvaed <edsonbr27@gmail.com>
2024-03-08 15:36:58 +0000

    c3

--- Dashboard.py
+++ Dashboard.py
@@ -1,6 +1,61 @@
-import streamlit as st
 import pandas as pd
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+import plotly.graph_objects as go
+import plotly.express as px
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from pandas.core.dtypes.cast import maybe_upcast
 from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.decomposition import IncrementalPCA
+import streamlit as st
+
+first_Run  = 0 
+
+while first_Run == 0:
+
+    dados = pd.read_csv('https://raw.githubusercontent.com/sthemonica/music-clustering/main/Dados/Dados_totais.csv')
+    dados_Alterados = dados.drop(["id","name","mode","artists_song","key","explicit"],axis=1)
+    ohe = OneHotEncoder(dtype=int)
+    colunas_ohe = ohe.fit_transform(dados_Alterados[['artists']]).toarray()
+    dados_Alterados_dummy = dados_Alterados
+    dados_Alterados_dummy = dados_Alterados_dummy.drop('artists',axis=1)
+    dados_Alterados_dummy = pd.concat([dados_Alterados_dummy, pd.DataFrame(colunas_ohe, columns=ohe.get_feature_names_out(['artists']))], axis=1)
+
+
+    SEED = 1224
+    np.random.seed(1224)
+    pca_pipeline = Pipeline([('scaler', StandardScaler()), ('PCA', PCA(n_components=0.7, random_state=SEED))])
+    music_embedding_pca = pca_pipeline.fit_transform(dados_Alterados_dummy)
+    projection = pd.DataFrame(data=music_embedding_pca)
+
+    kmeans_pca_pipeline = KMeans(n_clusters=50, verbose=False, random_state=SEED)
+
+    kmeans_pca_pipeline.fit(projection)
+
+    dados_Alterados['cluster_pca'] = kmeans_pca_pipeline.predict(projection)
+
+    dados_Alterados['artists_song'] = dados['artists_song']
+
+    dados_Alterados['id'] = dados['id']
+
+    projection['cluster_pca'] = kmeans_pca_pipeline.predict(projection)
+
+    projection['artist'] = dados['artists']
+
+    projection['artists_song'] = dados['artists_song']
+
+    projection['id'] = dados['id']
+
+    music_df = projection
+
+    first_Run = first_Run +1
+
+
+
 
 
 st.set_page_config(layout= 'wide')
@@ -11,7 +66,7 @@ st.markdown("<br>",unsafe_allow_html=True)
 
 
 
-music_df = pd.read_csv("df_Machine_Learning.csv")
+#music_df = pd.read_csv("df_Machine_Learning.csv")
 
 
 
@@ -56,14 +111,14 @@ def nome_da_Musica_Artista (nome_artista_Musica,df):
     musica_Selecionada_Y = musica_Selecionada_Y.iloc[0, 1]
 
     # Musica no mesmo cluster
-    musicas_recomendadas = df[ df['cluster_pca']== cluster] [[str(0), str(1), 'artists_song','id']]
-    distancias = euclidean_distances(musicas_recomendadas[[str(0), str(1)]],[[musica_Selecionada_X,musica_Selecionada_Y]])
+    musicas_recomendadas = df[ df['cluster_pca']== cluster] [[0, 1, 'artists_song','id']]
+    distancias = euclidean_distances(musicas_recomendadas[[0, 1]],[[musica_Selecionada_X,musica_Selecionada_Y]])
     musicas_recomendadas['Distancia'] = distancias
 
     musicas_recomendadas = musicas_recomendadas.sort_values('Distancia').head(11)
     musicas_recomendadas = musicas_recomendadas.reset_index(drop=True)
     musicas_recomendadas['link'] = 'https://open.spotify.com/track/'+musicas_recomendadas['id'].astype(str)
-    musicas_recomendadas = musicas_recomendadas.drop(['id','Distancia',str(0),str(1)],axis=1)
+    musicas_recomendadas = musicas_recomendadas.drop(['id','Distancia',0,1],axis=1)
     musicas_recomendadas = musicas_recomendadas[musicas_recomendadas['artists_song']!=nome_artista_Musica]
     musicas_recomendadas['link'] = musicas_recomendadas['link'].apply(make_clickable)
 
